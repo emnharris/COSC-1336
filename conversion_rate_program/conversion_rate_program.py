8f98cf43edb47698e353a367eeee7f602a32ac42
Emily Harris <emily.n.harris@gmail.com>
2019-09-30 11:50:25 -0500

    simple data validation added per Lab 3 instructions

--- conversion_rate_program/conversion_rate_program.py
+++ conversion_rate_program/conversion_rate_program.py
@@ -1,32 +1,45 @@
-print('IMPERIAL TO METRIC CALCULATOR\n')
+#defines constants for conversions
 
-# defines constants for conversion rates
+mi_to_km = 1.6
+gal_to_L = 3.9
+lb_to_kg = 0.45
+in_to_cm = 2.54
 
-mi_to_km = 1.6                                                             # conversion rate from miles to kilometres
-gal_to_L = 3.9                                                             # conversion rate from gallons to litres
-lb_to_kg = 0.45                                                            # conversion rate from pounds to kilograms
-inch_to_cm = 2.54                                                          # conversion rate from inches to centimetres
+#assigns input to variables
+#continuous data valiation; triggers termination as soon as unacceptable data is input
 
-# receives input for calculations; assigning variables
+mi = float(input('Miles: '))                                
+if (mi < 0):
+     print('Sorry, negative values not accepted.\nEND')                                                             #invalidates negative values                                                     
+else:
+     degF = float(input('\nTemperature (F): '))
+     if (degF > 1000):                                                                                              #invalidates data entered for temps above 1000 F
+          print('Sorry, value not accepted.\nEND')
+     else:
+          gal = float(input('\nGallons: '))
+          if (gal < 0):
+               print('Sorry, negative values not accepted.\nEND')
+          else:
+               lb = float(input('\nDry pounds: '))
+               if (lb < 0):
+                    print('Sorry, negative values not accepted.\nEND')
+               else:
+                    inches = float(input('\nInches: '))
+                    if (inches < 0):
+                         print('Sorry, negative values not accepted.\nEND') 
+                    else:                                                                                           #if all data is valid, triggers calculations
+                         km = mi * mi_to_km
+                         degC = (degF - 32) * (5/9)
+                         L = gal * gal_to_L
+                         kg = lb * lb_to_kg
+                         cm = inches * in_to_cm
 
-mi = float(input('Miles: '))
-degF = float(input('Degrees Fahrenheit: '))
-gal = float(input('Gallons: '))
-lb = float(input('Dry pounds: '))                                          # specifies dry pounds to avoid confusion with pounds sterling
-inch = float(input('Inches: '))
+#displays output
 
-# performs calculations
-
-km = mi * mi_to_km
-degC = (degF - 32) * (5/9)
-L = gal * gal_to_L
-kg = lb * lb_to_kg
-cm = inch * inch_to_cm
-
-# displays output for user to nearest tenth of a unit for neater, consistent viewing
-
-print('\n',mi,' miles = ',format(km,',.1f'),' kilometres',sep='')                          
-print(degF, 'degrees Fahrenheit =',format(degC,'.1f'),'degrees Celsius')   
-print(gal,'gallons =',format(L,',.1f'),'litres')
-print(lb,'pounds =',format(kg,',.1f'),'kilograms')
-print(inch,'inches =',format(cm,',.1f'),'centimetres')
+                         print('\n','*' * 30)
+                         print('\n',mi,' miles = ',format(km,'.1f'),' kilometres.\n',sep='')                
+                         print(degF,'degrees Fahrenheit =',format(degC,'.1f'),'degrees Centigrade.\n')
+                         print(gal,'gallons =',format(L,'.1f'),'litres.\n')
+                         print(lb,'pounds =',format(kg,'.1f'),'kilograms.\n')
+                         print(inches,'inches =',format(cm,'.1f'),'centimetres.\n')
+                         
