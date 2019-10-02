32281ade4437514114bb0f7bf8020895c6527839
Emily Harris <emily.n.harris@gmail.com>
2019-10-02 11:44:26 -0500

    add attempts to data validation, per Lab 4

--- conversion_rate_program/conversion_rate_program.py
+++ conversion_rate_program/conversion_rate_program.py
@@ -1,45 +1,78 @@
-#defines constants for conversions
+print('\n===== IMPERIAL TO METRIC CONVERSION CALCULATOR =====')
+print('=' * 52)
 
-mi_to_km = 1.6
-gal_to_L = 3.9
+#conversion constants
+mi_to_km = 1.60
+gal_to_L = 3.90
 lb_to_kg = 0.45
 in_to_cm = 2.54
+#attempt constant for limited attempt looping data validation
+attempt = 1
+#message constants
+TRY_AGAIN = '\nSorry, negative values are not accepted. Try again.\n'
+TERMINATE = '\nDATA INVALID\nGOODBYE'
 
-#assigns input to variables
-#continuous data valiation; triggers termination as soon as unacceptable data is input
-
-mi = float(input('Miles: '))                                
-if (mi < 0):
-     print('Sorry, negative values not accepted.\nEND')                                                             #invalidates negative values                                                     
+mi = float(input('Miles: '))
+while(mi < 0 and attempt < 3):
+    print(TRY_AGAIN)
+    print('You have',3 - attempt,'tries left.')
+    mi = float(input('Miles: '))
+    attempt = attempt + 1
+if(mi < 0 and attempt >= 3):    #if third try is still invalid, end program
+    print(TERMINATE)
 else:
-     degF = float(input('\nTemperature (F): '))
-     if (degF > 1000):                                                                                              #invalidates data entered for temps above 1000 F
-          print('Sorry, value not accepted.\nEND')
-     else:
-          gal = float(input('\nGallons: '))
-          if (gal < 0):
-               print('Sorry, negative values not accepted.\nEND')
-          else:
-               lb = float(input('\nDry pounds: '))
-               if (lb < 0):
-                    print('Sorry, negative values not accepted.\nEND')
-               else:
+    attempt = 1     #resets number of tries allowed
+    degF = float(input('\nTemperature (F): '))
+    while(degF > 1000 and attempt < 3):
+        print(TRY_AGAIN)
+        print('You have',3 - attempt,'tries left.')
+        degF = float(input('\nTemperature (F): '))
+        attempt = attempt + 1
+    if(degF > 1000 and attempt >= 3):
+        print(TERMINATE)
+    else:
+        attempt = 1
+        gal = float(input('\nGallons: '))
+        while(gal < 0 and attempt < 3):
+            print(TRY_AGAIN)
+            print('You have',3 - attempt,'tries left.')
+            gal = float(input('Gallons: '))
+            attempt = attempt + 1
+        if(gal < 0 and attempt >= 3):
+            print(TERMINATE)
+        else:
+            attempt = 1
+            lb = float(input('\nDry pounds: '))
+            while(lb < 0 and attempt < 3):
+                print(TRY_AGAIN)
+                print('You have',3 - attempt,'tries left.')
+                lb = float(input('\nDry pounds: '))
+                attempt = attempt + 1
+            if(lb < 0 and attempt >= 3):
+                print(TERMINATE)
+            else:
+                attempt = 1
+                inches = float(input('\nInches: '))
+                while(inches < 0 and attempt < 3):
+                    print(TRY_AGAIN)
+                    print('You have',3 - attempt,'tries left.')
                     inches = float(input('\nInches: '))
-                    if (inches < 0):
-                         print('Sorry, negative values not accepted.\nEND') 
-                    else:                                                                                           #if all data is valid, triggers calculations
-                         km = mi * mi_to_km
-                         degC = (degF - 32) * (5/9)
-                         L = gal * gal_to_L
-                         kg = lb * lb_to_kg
-                         cm = inches * in_to_cm
-
-#displays output
-
-                         print('\n','*' * 30)
-                         print('\n',mi,' miles = ',format(km,'.1f'),' kilometres.\n',sep='')                
-                         print(degF,'degrees Fahrenheit =',format(degC,'.1f'),'degrees Centigrade.\n')
-                         print(gal,'gallons =',format(L,'.1f'),'litres.\n')
-                         print(lb,'pounds =',format(kg,'.1f'),'kilograms.\n')
-                         print(inches,'inches =',format(cm,'.1f'),'centimetres.\n')
-                         
+                    attempt = attempt + 1
+                if(inches < 0 and attempt >= 3):
+                    print(TERMINATE)
+                else:
+                    #calculations run
+                    km = mi * mi_to_km
+                    degC = (degF - 32) * (5/9)
+                    L = gal * gal_to_L
+                    kg = lb * lb_to_kg
+                    cm = inches * in_to_cm
+                    #display output
+                    print('=======================RESULTS======================')
+                    print('=' * 52)
+                    print('\n',mi,' miles = ',format(km,'.1f'),' kilometres.\n',sep='')
+                    print(degF,'degrees Fahrenheit =',format(degC,'.1f'),'degrees Centigrade.\n')
+                    print(gal,'gallons =',format(L,'.1f'),'litres.\n')
+                    print(lb,'pounds =',format(kg,'.1f'),'kilograms.\n')
+                    print(inches,'inches =',format(cm,'.1f'),'centimetres.\n')
+                    
