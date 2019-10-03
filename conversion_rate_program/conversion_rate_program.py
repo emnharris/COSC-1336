7e7f109c6f69bc7771078f92941208b40371e212
Emily Harris <emily.n.harris@gmail.com>
2019-10-02 23:19:19 -0500

    removed magic number (3)

--- conversion_rate_program/conversion_rate_program.py
+++ conversion_rate_program/conversion_rate_program.py
@@ -7,61 +7,64 @@ gal_to_L = 3.90
 lb_to_kg = 0.45
 in_to_cm = 2.54
 #attempt constant for limited attempt looping data validation
-attempt = 1
+attempt = 0
+#max attempt constant
+max_attempt = 3
 #message constants
 TRY_AGAIN = '\nSorry, negative values are not accepted. Try again.\n'
 TERMINATE = '\nDATA INVALID\nGOODBYE'
 
+
 mi = float(input('Miles: '))
-while(mi < 0 and attempt < 3):
+while(mi < 0 and attempt < max_attempt):
     print(TRY_AGAIN)
-    print('You have',3 - attempt,'tries left.')
+    print('You have',max_attempt - attempt,'tries left.')
     mi = float(input('Miles: '))
     attempt = attempt + 1
-if(mi < 0 and attempt >= 3):    #if third try is still invalid, end program
+if(mi < 0 and attempt >= max_attempt):    #if third try is still invalid, end program
     print(TERMINATE)
 else:
-    attempt = 1     #resets number of tries allowed
+    attempt = 0     #reset number of tries allowed
     degF = float(input('\nTemperature (F): '))
-    while(degF > 1000 and attempt < 3):
+    while(degF > 1000 and attempt < max_attempt):
         print(TRY_AGAIN)
-        print('You have',3 - attempt,'tries left.')
+        print('You have',max_attempt - attempt,'tries left.')
         degF = float(input('\nTemperature (F): '))
         attempt = attempt + 1
-    if(degF > 1000 and attempt >= 3):
+    if(degF > 1000 and attempt >= max_attempt):
         print(TERMINATE)
     else:
         attempt = 1
         gal = float(input('\nGallons: '))
-        while(gal < 0 and attempt < 3):
+        while(gal < 0 and attempt < max_attempt):
             print(TRY_AGAIN)
-            print('You have',3 - attempt,'tries left.')
+            print('You have',max_attempt - attempt,'tries left.')
             gal = float(input('Gallons: '))
             attempt = attempt + 1
-        if(gal < 0 and attempt >= 3):
+        if(gal < 0 and attempt >= max_attempt):
             print(TERMINATE)
         else:
             attempt = 1
             lb = float(input('\nDry pounds: '))
-            while(lb < 0 and attempt < 3):
+            while(lb < 0 and attempt < max_attempt):
                 print(TRY_AGAIN)
-                print('You have',3 - attempt,'tries left.')
+                print('You have',max_attempt - attempt,'tries left.')
                 lb = float(input('\nDry pounds: '))
                 attempt = attempt + 1
-            if(lb < 0 and attempt >= 3):
+            if(lb < 0 and attempt >= max_attempt):
                 print(TERMINATE)
             else:
                 attempt = 1
                 inches = float(input('\nInches: '))
-                while(inches < 0 and attempt < 3):
+                while(inches < 0 and attempt < max_attempt):
                     print(TRY_AGAIN)
-                    print('You have',3 - attempt,'tries left.')
+                    print('You have',max_attempt - attempt,'tries left.')
                     inches = float(input('\nInches: '))
                     attempt = attempt + 1
-                if(inches < 0 and attempt >= 3):
+                if(inches < 0 and attempt >= max_attempt):
                     print(TERMINATE)
                 else:
-                    #calculations run
+                    #run calculations
                     km = mi * mi_to_km
                     degC = (degF - 32) * (5/9)
                     L = gal * gal_to_L
